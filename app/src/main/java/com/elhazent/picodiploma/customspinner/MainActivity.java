0bc5b9ac1eb8f8f562603cc2af99bf04949b6a83
elhazent <meeannawwaf007@gmail.com>
2019-04-05 14:57:04 +0700

    Custom Spinner

---
+++ app/src/main/java/com/elhazent/picodiploma/customspinner/MainActivity.java
@@ -0,0 +1,48 @@
+package com.elhazent.picodiploma.customspinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<CountryItem> mCountryList;
+    private CountryAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initList();
+
+        Spinner spinnerCountries = findViewById(R.id.spinner_id);
+
+        mAdapter = new CountryAdapter(this, mCountryList);
+        spinnerCountries.setAdapter(mAdapter);
+
+        spinnerCountries.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                CountryItem clickedItem = (CountryItem) parent.getItemAtPosition(position);
+                String clickedCountryName = clickedItem.getCountryName();
+                Toast.makeText(MainActivity.this, clickedCountryName + " selected", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void initList() {
+        mCountryList = new ArrayList<>();
+        mCountryList.add(new CountryItem("Indonesia", R.drawable.indo));
+        mCountryList.add(new CountryItem("Thailand", R.drawable.thail));
+    }
+}
